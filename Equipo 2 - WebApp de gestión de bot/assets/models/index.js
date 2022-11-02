d071de4fea219e9c425aa25e194f0f3b27ac1fbb
MullerDerRaumdeuter <a01745219@itesm.mx>
2022-11-01 23:02:27 -0600

    ya puedo insertar la ptm

--- Equipo 2 - WebApp de gestión de bot/assets/models/index.js
+++ Equipo 2 - WebApp de gestión de bot/assets/models/index.js
@@ -1,23 +1,37 @@
-import { config } from './model.mjs';
-import { Sequelize, Model, DataTypes, QueryTypes} from 'sequelize';
 
+var mysql = require('mysql');
 
-const sequelize = new Sequelize(config.database, config.username, '', {
-    host: config.host,
-    dialect: config.dialect
+const config = {
+  database: 'interacciones',
+  username: 'root',
+  password: '',
+  host: '35.247.112.172',
+  //port: '5001',
+  dialect: 'mysql'
+}
+
+var connection = mysql.createConnection({
+  host: config.host,
+  user: config.username,
+  password: config.password,
+  database: config.database
+})
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql_insert = `insert into interacciones (id, etiqueta, tipo, categoria, preguntas, respuestas, sucesor_de, link)
+                    values (37, "hoja33", "link", "cat6", '[{"pregunta1":"Fernando Palomo", "pregunta2":"Andres Agulla"}]', '[{"respuesta":"Amigos del Futbol"}]', 5, "https://www.youtube.com/watch?v=ZBOPMHzqZ78&ab_channel=AmigosdelF%C3%BAtbol");`
+  connection.query(sql_insert, function(err, result){
+    if(err) throw err;
+    console.log(result)
+  });
+  var sql_get = "select * from interacciones"
+  connection.query(sql_get, function(err, result){
+    if(err) throw err;
+    console.log(result)
+  });
 });
 
-try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-/*const interacciones = 
-const inter = await interacciones.findAll();
-console.log(inter.every(inter => inter instanceof interacciones));
-console.log("All:", JSON.stringify(inter, null, 2));*/
 
-const results = await sequelize.query("SELECT * FROM interacciones;", {type: QueryTypes.SELECT});
-console.log(results)
 
