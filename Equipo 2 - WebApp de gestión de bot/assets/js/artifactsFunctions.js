10fc1c9f1ff395d537cfc38abb2d077aa66b7c71
Sebastian Morales Martín <sebfut@hotmail.com>
2022-11-14 23:09:00 -0600

    try catches

--- Equipo 2 - WebApp de gestión de bot/assets/js/artifactsFunctions.js
+++ Equipo 2 - WebApp de gestión de bot/assets/js/artifactsFunctions.js
@@ -1,7 +1,7 @@
 //dropdown menu data and creation
 
 var isCreate = true
-var data = ["Conversación","Soporte", "Innovacion", "Obras públicas", "Servicios Públicos", "SAPASA", "Seguridad Pública", "Desarrollo Urbano", "Contraloría Municipal", "Protección Civil", "Normatividad", "Subdirección de Tránsito", "Desarrollo Social", "Desarrollo Económico", "Derechos Humanos", "Seguridad Pública y Tránsito", "Secretaría General", "Tesoreria", "Servicios Jurídicos", "Instituto de la Mujer", "Educación", "Juventud", "DIF", "Jurídico", "Presidencia"]
+var data = ["Conversación", "Soporte", "Innovacion", "Obras públicas", "Servicios Públicos", "SAPASA", "Seguridad Pública", "Desarrollo Urbano", "Contraloría Municipal", "Protección Civil", "Normatividad", "Subdirección de Tránsito", "Desarrollo Social", "Desarrollo Económico", "Derechos Humanos", "Seguridad Pública y Tránsito", "Secretaría General", "Tesoreria", "Servicios Jurídicos", "Instituto de la Mujer", "Educación", "Juventud", "DIF", "Jurídico", "Presidencia"]
 var dropdown = document.getElementById('categoria');
 data.forEach(function (row) {
     var option = document.createElement('option');
@@ -96,9 +96,9 @@ function createTable() {
                     button.setAttribute('data-bs-toggle', 'modal')
                     button.setAttribute('data-bs-target', '#modalRamas')
                     button.appendChild(document.createTextNode('Ver Ramas'))
-//Implements the functions that the button will do after clicked,
-// in this case, this will hide the aside bar to show all the interactions
-// on a modal which will retrieve from a query it's required data
+                    //Implements the functions that the button will do after clicked,
+                    // in this case, this will hide the aside bar to show all the interactions
+                    // on a modal which will retrieve from a query it's required data
                     button.addEventListener('click', function () {
                         console.log(button.id)
                         genrateTreeTable(button.id)
@@ -282,6 +282,8 @@ function editArtifact(etiqueta) {
 
     fetch(url).then((result) => result.json())
         .then((data) => {
+            console.log(data)
+
             var tag = document.getElementById('etiqueta')
             var type = document.getElementById('tipo')
             var cat = document.getElementById('categoria')
@@ -358,7 +360,6 @@ function cancelEdit(button1) {
 
 function handleSubmit(event) {
     event.preventDefault();
-    console.log(event)
     const data = new FormData(event.target);
     const value = Object.fromEntries(data.entries());
     value.link = document.getElementById('link').value
@@ -373,19 +374,49 @@ function handleSubmit(event) {
     value.patrones = cleansedPreguntas;
     value.respuestas = cleansedRespuestas;
     value.etiqueta = document.getElementById('etiqueta').value
-    if (isCreate) {
-        console.log('creating')
-        console.log(value)
-        postCreate(value)
+    //Check constraints, first: the tag shouldn't be empty, 
+    //                   second, there must be at least one question AND one answer
+    if (value.etiqueta != '') {
+        if (value.patrones.length < 1 || value.respuestas.length < 1) {
+            createAlert('Debes subir al menos una pregunta y una respuesta', 'warning')
+        } else {
+            if (isCreate) {
+                console.log('creating')
+                postCreate(value)
+            } else {
+                console.log('updating')
+                postUpdate(value)
+            }
+        }
+
     } else {
-        console.log('updating')
-        postUpdate(value)
+        createAlert('La etiqueta no puede estar vacia', 'warning');
     }
 
 
 
 
 
+
+
+
+
+}
+
+function createAlert(message, severity) {
+    var alert = document.createElement('div')
+    var alertSeverity = 'alert-' + severity
+    alert.className = 'alert alert-dismissable ' + alertSeverity
+    alert.setAttribute('role', 'alert')
+    alert.appendChild(document.createTextNode(message))
+    //<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    var button = document.createElement('button')
+    button.type = 'button'
+    button.className = 'btn btn-close'
+    button.setAttribute('data-bs-dismiss', 'alert')
+    alert.appendChild(button)
+    modal = document.getElementById('modalInt-footer')
+    modal.appendChild(alert)
 
 }
 
@@ -400,7 +431,7 @@ function postUpdate(value) {
             .then(data => console.log(data))
     }, 500)
     //Reloads the page so the update on the database will be seen on the artifacts main page
-
+    showAside()
     isCreate = true
     setTimeout(() => document.location.reload(), 1000)
 }
@@ -418,7 +449,7 @@ function postCreate(value) {
     }, 500)
 
     console.log(JSON.stringify(value))
-
+    showAside()
     //Reloads the page so the update on the database will be seen on the artifacts main page
     setTimeout(() => document.location.reload(), 1000)
 }
