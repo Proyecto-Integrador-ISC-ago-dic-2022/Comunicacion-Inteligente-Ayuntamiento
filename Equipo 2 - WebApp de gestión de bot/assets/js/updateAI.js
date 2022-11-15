66a2d0921bdce3fc4c500eca250785651a638ae3
Sebastian Morales Martín <sebfut@hotmail.com>
2022-11-14 20:37:17 -0600

    Boton para actualizar bot

--- Equipo 2 - WebApp de gestión de bot/assets/js/updateAI.js
+++ Equipo 2 - WebApp de gestión de bot/assets/js/updateAI.js
@@ -1,16 +1,16 @@
 function updateAI() {
+    console.log('updating')
     fetch('http://127.0.0.1:8080/artefactos/readForAI').then(function (response) {
-        console.log(response)
         return response.json()
     }).then(function (data) {
-        /*fetch('http://127.0.0.1:5000/update', {
+        fetch('http://127.0.0.1:5000/update', {
             method: 'POST',
             headers: {'Content-Type': 'appliaction/json'},
             body: JSON.stringify(data),
 
         }).then(raw => raw.json()).then(data => console.log(data))
-        .catch(err=> console.log(err))*/
-        console.log(JSON.stringify(data))
+        .catch(err=> console.log(err))
+        console.log(data)
 
     }).catch(function (err) {
         console.log(err)
