77471aa01b06f4f23dd85d3e75314a89e9b6d266
HowlandAlek <howlandalek@gmail.com>
2022-11-22 21:44:58 -0600

    Se bufeo a la Zarati

--- Equipo 1 - Chatbot/API/train.py
+++ Equipo 1 - Chatbot/API/train.py
@@ -68,11 +68,11 @@ def train(intents):
         y_train = np.array(y_train)
 
         # Hyper-parameters 
-        num_epochs = 1000
-        batch_size = 8
+        num_epochs = 2000
+        batch_size = 999
         learning_rate = 0.001
         input_size = len(X_train[0])
-        hidden_size = 8
+        hidden_size = 999
         output_size = len(tags)
         print(input_size, output_size)
 
